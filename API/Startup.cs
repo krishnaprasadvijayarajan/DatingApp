f2ca31dd383c52b733343a1356ee86a71e2398ac
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-08 21:41:14 +0000

    End of Module 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -62,7 +62,7 @@ namespace API
 
             app.UseRouting();
             // app.UseCors("test");
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
