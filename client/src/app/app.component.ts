b734cd7d1a44c6758ddc3ffc8d9d41386e13d79e
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-10 21:55:19 +0000

    Section 5 in progress

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { setTheme } from 'ngx-bootstrap/utils';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,11 +13,17 @@ export class AppComponent implements OnInit {
   description='This is a demo App';
   users:any;
 
-  constructor(private http:HttpClient){
+  constructor(private http:HttpClient, private accountService: AccountService){
 
   }
   ngOnInit() {
     this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const user:User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
   getUsers(){
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
