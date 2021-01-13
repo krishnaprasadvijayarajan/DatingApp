93c17147e44c5e94cd7f67c94132270da7833de4
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-13 22:35:22 +0000

    Parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,11 +13,10 @@ export class AppComponent implements OnInit {
   description='This is a demo App';
   users:any;
 
-  constructor(private http:HttpClient, private accountService: AccountService){
+  constructor( private accountService: AccountService){
 
   }
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -25,11 +24,5 @@ export class AppComponent implements OnInit {
     const user:User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
   }
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users=response;
-    },error=>{
-      console.log(error);
-    });
-  }
+
 }
