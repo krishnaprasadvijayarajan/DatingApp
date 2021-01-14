492555e4895636e84af0fc5d5658f02b30bb2884
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-14 20:22:47 +0000

    End of Module 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -23,7 +23,6 @@ export class AccountService {
             localStorage.setItem('user',JSON.stringify(user));
             this.currentUserSource.next(user);
           }
-          return user;
         })
       )
     }
