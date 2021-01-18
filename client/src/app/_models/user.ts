8c63d3f190d1d63668de3f7747be2f30720b5fbc
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-18 22:53:51 +0000

    Module 9 - WIP 1 - Building UI

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,29 @@ export interface User{
   username:string;
   token:string;
 }
+
+
+
+//DEMO
+// let data: number | string = 1;
+// data ="welcome"
+
+// interface car {
+//   color:string,
+//   model:string,
+//   topSpeed?:number
+// }
+// const car1:car ={
+//   color:'blue',
+//   model:'BMW'
+// }
+
+// const car2:car ={
+//   color:'silver',
+//   model:'Mercedes',
+//   topSpeed:100
+// }
+
+// const multiply = (x,y)=>{
+//   return x*y;
+// }
